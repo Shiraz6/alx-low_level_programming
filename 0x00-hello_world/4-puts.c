7b44f3983837e0fa6bc3bdc834abcf28c4415f76
shiraz6 <rosemohamed246@gmail.com>
2023-09-07 18:03:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - this is the main function
  *
- * Return: always (0)
+ * Description: A C proogram that prints with put function
  *
+ * Return: always (0)
  */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
-	return (0);	 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
